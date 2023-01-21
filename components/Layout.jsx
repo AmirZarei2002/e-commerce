48f232a67d37f3e967f316a044b12d418f229fe0
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-01-21 20:18:10 +0330

    some spaces added

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,23 +5,25 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function Layout({ title, children }) {
-  return (
-    <>
-      <Head>
-        <title>{title ? title + '- E-commerce' : 'E-commerce'}</title>
-        <meta name="description" content="E-commerce Website" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <ToastContainer position="bottom-center" limit={1} />
-      <div className="flex min-h-screen flex-col justify-between">
-        <header>
-          <Navbar />
-        </header>
-        <main className="container m-auto mt-3 mb-4 px-4">{children}</main>
-        <footer>
-          <Footer />
-        </footer>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>{title ? title + '- E-commerce' : 'E-commerce'}</title>
+                <meta name="description" content="E-commerce Website" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <ToastContainer position="bottom-center" limit={1} />
+            <div className="flex min-h-screen flex-col justify-between">
+                <header>
+                    <Navbar />
+                </header>
+                <main className="container m-auto mt-3 mb-4 px-4">
+                    {children}
+                </main>
+                <footer>
+                    <Footer />
+                </footer>
+            </div>
+        </>
+    );
 }
